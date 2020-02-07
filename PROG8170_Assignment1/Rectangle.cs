d45949fe312cbb74e458a7b25e227f6709fc2c42
chashamdeep kaur <ckaur8336@conestogac.on.ca>
2020-02-06 20:05:30 -0500

    rectangle class created

--- PROG8170_Assignment1/Rectangle.cs
+++ PROG8170_Assignment1/Rectangle.cs
@@ -6,7 +6,51 @@ using System.Threading.Tasks;
 
 namespace PROG8170_Assignment1
 {
-    class Rectangle
+    public class Rectangle
     {
+        //member variables
+        private int length;
+        private int width;
+        public Rectangle()
+        {
+            this.length = 1;
+            this.width = 1;
+        }
+        public Rectangle(int length, int width)
+        {
+            this.length = length;
+            this.width = width;
+        }
+
+        public int GetLength()
+        {
+            return this.length;
+        }
+
+        public int SetLength(int length)
+        {
+            this.length = length;
+            return this.length;
+        }
+
+        public int GetWidth()
+        {
+            return this.width;
+        }
+
+        public int SetWidth(int width)
+        {
+            this.width = width;
+            return this.width;
+        }
+        public int GetArea()
+        {
+            return (this.length * this.width);
+        }
+
+        public int GetPerimeter()
+        {
+            return (2 * (this.length + this.width));
+        }
     }
 }
